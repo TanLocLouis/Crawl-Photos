56469a0485133d8ba2e7442961fe09b11af76359
CBT-TanLoc <74963205+CBT-TanLoc@users.noreply.github.com>
2022-07-08 15:57:07 +0700

    Add files via upload

--- Crawl.cpp
+++ Crawl.cpp
@@ -6,32 +6,37 @@
 using namespace std;
 
 int main() {
-    int count;
-    string text, img, file, find_img = "<img src=\"https://camo";
+    int count, pages, j;
+    string url, text, img, file, find_img = "<img src=\"https://voz.vn/attachment";
 
+    cout << "Link: ";
+    cin >> url;
+    cout << "Number of Pages: ";
+    cin >> pages;
     //system("curl https://voz.vn/t/thu-tuong-anh-chap-nhan-tu-chuc.579379/page-1.html >> VOZ.txt");
     //https://voz.vn/attachments/1639063273139-png.913705/
     //if (text[i] == char(34) && text[i + 1] == char(32)) {
-again:
-    cout << "File name: ";
-    cin >> file;
-    fstream f1(file + ".txt");
 
-    while (getline(f1, text)){
-        if (text.find(find_img) != string::npos) {
-            for (int i = 0; i <= text.length() - 2; i++) {
-                if (text[i] == char(34) && text[i + 1] == char(32)) {
-                    img = text.substr(10,i - 10);
-                    time_t now = time(0);
-                    system(("wget --user-agent=\"Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0\"" + img + " -O Picture_" + to_string(now) + ".jpg").c_str());
-                    count++;
-                    cout << img << endl;
-                    cout << endl;
-                    break;
-                }             
+    for (int j = 1; j <= pages; j++) {
+         system(("wget -O VOZ.txt --user-agent=\"Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0\" " + url + "page-" + to_string(j)).c_str());
+
+        fstream f1("VOZ.txt");
+
+        while (getline(f1, text)){
+            if (text.find(find_img) != string::npos) {
+                for (int i = 0; i <= text.length() - 2; i++) {
+                    if (text[i] == char(34) && text[i + 1] == char(32)) {
+                        img = text.substr(10,i - 10);
+                        time_t now = time(0);
+                        system(("wget --user-agent=\"Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0\" " + img + " -O Photo_" + to_string(now) + ".jpg").c_str());
+                        count++;
+                        cout << img << endl;
+                        cout << endl;
+                        break;
+                    }             
+                }
             }
         }
+        f1.close();
     }
-    f1.close();
-    goto again;
 }

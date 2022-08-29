337eb0160054daf99b91eed77d19c2420df9f5ff
CBT-TanLoc <74963205+CBT-TanLoc@users.noreply.github.com>
2022-08-29 17:54:06 +0700

    Fix put commit() outside to inside && Change time counting photo to number

--- Crawl.cpp
+++ Crawl.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <ctime>
 
 using namespace std;
 
-int count, pages, each_page;
+int count = 0, pages, each_page;
 string url, text, img, file, find_img = "<img src=\"https://voz.vn/attachment"; // pattern to dectect data
 
 void input() {
@@ -13,12 +12,6 @@ void input() {
     cin >> url;
     cout << "Number of Pages: ";
     cin >> pages;
-}
-
-void get_data() {
-        for (int each_page = 1; each_page <= pages; each_page++) { // get data each page
-        system(("wget -O VOZ.txt --user-agent=\"Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0\" " + url + "page-" + to_string(each_page)).c_str()); //download current page
-        }
 }
 
 void commit() {
@@ -29,10 +22,10 @@ void commit() {
                 for (int i = 0; i <= text.length() - 2; i++) {
                     if (text[i] == char(34) && text[i + 1] == char(32)) {
                         img = text.substr(10,i - 10);
-                        time_t now = time(0);
-                        system(("wget --user-agent=\"Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0\" " + img + " -O Photo_" + to_string(now) + ".jpg").c_str()); // download data
+                        system(("wget --user-agent=\"Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0\" " + img + " -O Photo_" + to_string(count) + ".jpg").c_str()); // download data
                         count++;
                         cout << img << endl; // show link of data
+                        cout << "------------------------------------------------------------------" << endl;
                         cout << endl;
                         break;
                     }             
@@ -42,8 +35,14 @@ void commit() {
         f1.close();
 }
 
+void get_data() {
+        for (int each_page = 1; each_page <= pages; each_page++) { // get data each page
+            system(("wget -O VOZ.txt --user-agent=\"Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0\" " + url + "page-" + to_string(each_page)).c_str()); //download current page
+            commit();
+        }
+}
+
 int main() {
     input();
     get_data();
-    commit();
 }
